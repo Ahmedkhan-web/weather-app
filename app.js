c94b4a94d545aa0085ce3cf6999efaeca618d3ec
Ahmedkhan-web <a.khan.786.786.786.7@gmail.com>
2026-01-03 12:03:48 -0800

    weather app

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
-const api = "https://api.weatherapi.com/v1/current.json?key=API_KEY&q=karachi&aqi=no";
 const apikey = "12357f9b3b4a4c6b90c181353260301";
 
 const form = document.querySelector("#form");
 const input = document.querySelector("#input");
+const div = document.querySelector(".div");
 
 form.addEventListener("submit" , (e) =>{
     e.preventDefault();
-    console.log(input.value)
-});
+    
+    const city = input.value;
 
-fetch(`https://api.weatherapi.com/v1/current.json?key=${apikey}&q=${input.value}&aqi=no`)
+
+fetch(`https://api.weatherapi.com/v1/current.json?key=${apikey}&q=${city}&aqi=no`)
      .then((res) => res.json())
      .then((res) => {
         console.log(res)
+        div.innerHTML = `<h2>${res.location.name}, ${res.location.country}</h2>
+        <h2>Temperature: ${res.current.temp_c}</h2>`
     })
     .catch((err) => {
         console.log(err);
         
     })
+});
